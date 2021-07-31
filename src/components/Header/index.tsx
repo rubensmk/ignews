3e2e4c3f5368fe0d01894f41f4938801f4fab510
rubensmk <rubenskishimoto@gmail.com>
2021-07-31 15:34:44 -0300

    feat: Finished :sparkles:

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import { SignInButton } from '../SignInButton'
+import Link from 'next/link';
+import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
